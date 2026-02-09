8e7e70f0bd9bf2e6cd52c333498cbba56dd3172b
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-09 16:47:08 +0330

    feat: connect the app to atlas DB

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require("path"); // 1. Import the path module
+
+// 2. Use path.join with __dirname to force the correct location
+dotenv.config({
+  path: path.join(__dirname, "config.env"),
+});
+
 const app = require("./app");
 
-dotenv.config({
-  path: "./config.env",
-});
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false, // Note: standard is usually false here
+  })
+  .then(() => console.log("DB connection Successful"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
