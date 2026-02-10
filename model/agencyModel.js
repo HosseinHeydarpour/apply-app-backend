dd9b96b74ad34a0064dac234ef9e1463b5f75718
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-10 11:40:03 +0330

    feat: add slugs to the agency schema

--- model/agencyModel.js
+++ model/agencyModel.js
@@ -1,19 +1,37 @@
 const mongoose = require("mongoose");
 
-const agencySchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  logoUrl: { type: String, trim: true },
-  description: { type: String, trim: true },
-  contactInfo: { type: String, trim: true }, // یا آبجکت شامل تلفن و ایمیل
+const agencySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    logoUrl: { type: String, trim: true },
+    description: { type: String, trim: true },
+    contactInfo: { type: String, trim: true }, // یا آبجکت شامل تلفن و ایمیل
+    slug: { type: String, trim: true },
+    // ارتباط با دانشگاه‌ها (این موسسه کدام دانشگاه‌ها را ساپورت می‌کند)
+    supportedUniversities: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "University",
+      },
+    ],
+    createdAt: { type: Date, default: Date.now, select: false },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
 
-  // ارتباط با دانشگاه‌ها (این موسسه کدام دانشگاه‌ها را ساپورت می‌کند)
-  supportedUniversities: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "University",
-    },
-  ],
-  createdAt: { type: Date, default: Date.now, select: false },
+// agencySchema.virtual("universities", {
+//   ref: "University",
+//   localField: "supportedUniversities",
+//   foreignField: "_id",
+// });
+
+// Generate slug from name before saving
+agencySchema.pre("save", function (next) {
+  this.slug = require("slugify")(this.name, { lower: true });
+  next();
 });
 
 const Agency = mongoose.model("Agency", agencySchema);
