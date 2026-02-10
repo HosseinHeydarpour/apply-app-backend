a8990ca30695f0d4be026e4116a0a77157adc66e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-10 17:35:05 +0330

    feat: implement full error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,65 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: ${err.keyValue.name}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const message = Object.values(err.errors).map((el) => el.message);
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    messasge: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak details
+  } else {
+    res.status(err.statusCode).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    // keep the original error message
+    error.message = err.message;
+    error.name = err.name;
+    error.code = err.code;
+    error.errors = err.errors;
+    error._message = err._message;
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path"); // 1. Import the path module
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // 2. Use path.join with __dirname to force the correct location
 dotenv.config({
   path: path.join(__dirname, "config.env"),
@@ -23,6 +29,12 @@ mongoose
   .then(() => console.log("DB connection Successful"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err);
+  server.close(() => process.exit(1));
 });
