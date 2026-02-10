5a483ab6f03b9fb5755d9bba14de7c56f3023bac
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-10 17:43:34 +0330

    feat: add confirm password for user

--- model/userModel.js
+++ model/userModel.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
 
+const validator = require("validator");
+
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, "First name is required"],
       trim: true,
       maxlength: [50, "First name cannot be more than 50 characters"],
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, "Last name is required"],
       trim: true,
       maxlength: [50, "Last name cannot be more than 50 characters"],
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validator: [validator.isEmail, "Please provide a valid email"],
     },
     phone: {
       type: String,
@@ -21,7 +31,11 @@ const userSchema = new mongoose.Schema(
       trim: true,
       maxlength: [20, "Phone cannot be more than 20 characters"],
     }, // کلید اصلی برای لاگین
-    password: { type: String, required: true }, // حتما هش شده ذخیره شود
+    password: { type: String, required: [true, "Password is required"] }, // حتما هش شده ذخیره شود
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+    },
     profileImage: { type: String, trim: true }, // آدرس تصویر پروفایل
 
     // مدارک به صورت آرایه ذخیره می‌شوند تا قابلیت گسترش داشته باشد
