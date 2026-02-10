033183174e409368af42b4722b261c62996b1b52
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-10 18:14:35 +0330

    feat: hash passwords with bcryt js

--- model/userModel.js
+++ model/userModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const validator = require("validator");
 
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -35,6 +37,12 @@ const userSchema = new mongoose.Schema(
     passwordConfirm: {
       type: String,
       required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords are not the same",
+      },
     },
     profileImage: { type: String, trim: true }, // آدرس تصویر پروفایل
 
@@ -70,6 +78,17 @@ const userSchema = new mongoose.Schema(
 // make full name
 userSchema.virtual("fullName").get(function () {
   return `${this.firstName} ${this.lastName}`;
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password);
+
+  // Delete password confirm
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
