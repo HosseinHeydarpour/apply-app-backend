35bfd45a48ac741e46308ccf861dadd2536b2c1f
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-11 16:42:33 +0330

    fix: fix duplicate user field

--- model/userModel.js
+++ model/userModel.js
@@ -70,7 +70,6 @@ const userSchema = new mongoose.Schema(
         uploadedAt: { type: Date, default: Date.now },
       },
     ],
-    profilePicture: { type: String, trim: true, default: "" }, // آدرس تصویر پروفایل
 
     createdAt: { type: Date, default: Date.now, select: false },
 
